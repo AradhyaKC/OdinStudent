b37f7df70973dff0ee2713672afb0cbcac7bfaa3
Aradhya KC <arukc47@gmail.com>
2022-08-11 19:48:28 +0530

    added wepvack

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import './style.css'
+import _ from 'lodash';
+import myImage from "./cross.svg";
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add("hello");
+    const myIcon = new Image();
+
+    myIcon.src = myImage;
+    console.log("hello ");
+    cosnole.log("hello this ");
+  
+  
+    element.appendChild(myIcon);
+    return element;
+}
+  
+  document.body.appendChild(component());
