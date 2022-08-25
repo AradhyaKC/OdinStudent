81c9d8442d499fd0f6a0e4efbddf8f59deafbb4c
AradhyaKC <arukc47@gmail.com>
2022-08-25 14:12:09 +0530

    finished memory card essential features

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -64,81 +64,103 @@ import { useState } from 'react';
 // }
 
 
+// function App(props){
+//   var [count, setCount] = useState(6);
+//   const incrementCount= ()=>{
+//     setCount(count+1);
+//   }
+
+//   var copycat=2;
+//   useEffect(()=>{
+//     copycat=count;
+//     console.log(copycat);
+//   },[count]); 
+
+//   return (<div>
+//     <div> hello World , the count is {count}
+//     <div>copycat = {copycat!=undefined && <div>{copycat}</div>}</div> 
+//     </div>
+//     <button onClick={incrementCount}> incrementCount </button>
+//     <GeneralInfoFunc/>
+//   </div>
+//   );
+// }
+
+// function GeneralInfoFunc(props){
+//   const [state,setState]=useState({
+//     name:'',Age:0,onEditClicked:false
+//   });
+
+//   var nameElement,ageElement;
+//   useEffect(()=>{
+//     nameElement = document.querySelector('#name');
+//     ageElement=document.querySelector("#age");
+//     console.log("should be called everytime the page or name/age Elements re-render");
+//   },[state]);
+
+//   const onClick=(e)=>{
+//     e.preventDefault();
+//     if(nameElement.validity.valid && ageElement.validity.valid){
+//       setState({
+//         name:nameElement.value, age:ageElement.value, onEditClicked:false
+//       });
+//     }
+//   }
+//   const onEditClick=(e)=>{
+//     e.preventDefault();
+//     if(state.onEditClicked==false){
+//       console.assert(state.name!='' && state.age>0);
+//       nameElement.setAttribute('placeholder', state.name);
+//       ageElement.setAttribute('placeholder',state.age);
+//       nameElement.value='';
+//       ageElement.innerHTML='';
+//       setState({onEditClicked:true});
+//     }else{
+//       onClick(e);
+//     }
+//   } 
+
+//   return (
+//   <div>
+//     <form>
+//         <div>
+//           <div> <label htmlFor='name' >name</label></div>
+//           <div> <input  type='text' name='name' id='name' placeholder='name'/></div>
+//           <div> <label htmlFor='age'> Age</label></div>
+//           <div> <input type='number' name='age' id='age' placeholder='0'/></div>
+//           {state.onEditClicked==false && <button onClick={onClick} type='button'> Click</button>}
+//           { state.name !='' && <button onClick={onEditClick}> {state.onEditClicked?'Submit Edit':'Edit'}</button>}
+//         </div>
+//         <h1>
+//           <div>display :{state.name} and  age:{state.age}</div>
+//         </h1>
+//     </form>
+
+//   </div>
+  
+//   );
+// }
+
 function App(props){
-  var [count, setCount] = useState(6);
-  const incrementCount= ()=>{
-    setCount(count+1);
-  }
-
-  var copycat=2;
-  useEffect(()=>{
-    copycat=count;
-    console.log(copycat);
-  },[count]); 
-
-  return (<div>
-    <div> hello World , the count is {count}
-    <div>copycat = {copycat!=undefined && <div>{copycat}</div>}</div> 
-    </div>
-    <button onClick={incrementCount}> incrementCount </button>
-    <GeneralInfoFunc/>
-  </div>
-  );
-}
-
-function GeneralInfoFunc(props){
-  const [state,setState]=useState({
-    name:'',Age:0,onEditClicked:false
+  const [state,setState] = useState(()=>{
+    var ClickArray=[];
+    for(let i=0;i<20;i++){ ClickArray.push(false);}
+    console.log("should be called only once");
+    return {ClickArray:ClickArray};
   });
 
-  var nameElement,ageElement;
-  useEffect(()=>{
-    nameElement = document.querySelector('#name');
-    ageElement=document.querySelector("#age");
-    console.log("should be called everytime the page or name/age Elements re-render");
-  },[state]);
-
-  const onClick=(e)=>{
-    e.preventDefault();
-    if(nameElement.validity.valid && ageElement.validity.valid){
-      setState({
-        name:nameElement.value, age:ageElement.value, onEditClicked:false
-      });
-    }
+  const onDivClick= async (key)=>{
+    await setState((prevState=>{ 
+      prevState.ClickArray[key]=true;
+      return {...prevState};
+    }));
   }
-  const onEditClick=(e)=>{
-    e.preventDefault();
-    if(state.onEditClicked==false){
-      console.assert(state.name!='' && state.age>0);
-      nameElement.setAttribute('placeholder', state.name);
-      ageElement.setAttribute('placeholder',state.age);
-      nameElement.value='';
-      ageElement.innerHTML='';
-      setState({onEditClicked:true});
-    }else{
-      onClick(e);
-    }
-  } 
-
   return (
-  <div>
-    <form>
-        <div>
-          <div> <label htmlFor='name' >name</label></div>
-          <div> <input  type='text' name='name' id='name' placeholder='name'/></div>
-          <div> <label htmlFor='age'> Age</label></div>
-          <div> <input type='number' name='age' id='age' placeholder='0'/></div>
-          {state.onEditClicked==false && <button onClick={onClick} type='button'> Click</button>}
-          { state.name !='' && <button onClick={onEditClick}> {state.onEditClicked?'Submit Edit':'Edit'}</button>}
-        </div>
-        <h1>
-          <div>display :{state.name} and  age:{state.age}</div>
-        </h1>
-    </form>
-
-  </div>
-  
+    <div className='grid-container App'>
+      {console.log('rendering ' +state.ClickArray[9].toString()) }
+      {state.ClickArray.map((element,index)=> <div onClick={()=>{onDivClick(index);}} key={index}
+       className={state.ClickArray[index]?"grid-element clicked":"grid-element"}> Hello World {index+1} </div>)}
+    </div>
   );
 }
-
 export default App;
